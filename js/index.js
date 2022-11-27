4d0c1208b7a04a42a171ac484db39f4a17eaecd8
LordWhiskas <102185030+LordWhiskas@users.noreply.github.com>
2022-11-27 17:39:28 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 import Router from "./paramHashRouter.js";
 import Routes from "./routes.js";
-
 window.router = new Router(Routes,"welcome");
+document.getElementById("wel").onclick= function (){
+    document.getElementById("art").classList = null;
+    document.getElementById("wel").classList = "active";
+    document.getElementById("opi").classList = null;
+    document.getElementById("add").classList = null;
+}
